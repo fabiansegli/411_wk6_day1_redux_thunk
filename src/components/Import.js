b7f2fdd048b110b13b6978874000adcff38482b6
unknown <seglifabian@gmail.com>
2020-03-08 18:02:38 -0500

    Assigment Finalized

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import DeleteForeverOutlinedIcon from "@material-ui/icons/DeleteForeverOutlined";
+import Menu from "@material-ui/core/Menu";
+import {
+  Button,
+  Container,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
+} from "@material-ui/core";
 
-const Import = (props) => {
-    // fill out this component
+const Import = props => {
+    
+  return (
+    <div>
+      <Container style={{ marginTop: "40px" }}>
+        <Button onClick={props.fetchMakes} variant="contained" color="primary">
+          Import
+        </Button>
+        <h2>Count: {props.makes.length}</h2>
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">ID</TableCell>
+            <TableCell align="left">Make</TableCell>
+            <TableCell align="left">Actions</TableCell>
+          </TableRow>
+        </TableHead>
 
-    return (
-        <p>Import Component</p>
-    )
-}
+        <TableBody>
+          {props.makes.map((make, index) => {
+            return (
+              <TableRow key={make.MakeId}>
+                <TableCell align="left">{make.MakeId}</TableCell>
+                <TableCell align="left">{make.MakeName}</TableCell>
+                <TableCell align="left">
+                  <DeleteForeverOutlinedIcon
+                    className="delete-button"
+                    onClick={() => props.deleteMake(index)}
+                  />
+                  <Menu />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Container>
+    </div>
+  );
+};
 
-export default Import
+export default Import;
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -16,10 +16,16 @@ const cars = (state = [], action) => {
 }
 
 const makes = (state = [], action) => {
-    switch(action.type) {
-        default:
-            return state
+    switch (action.type) {
+      case "FETCH_MAKES":
+        return action.value;
+      case "DELETE_MAKE":
+        const makes = [...state];
+        makes.splice(action.value, 1);
+        return makes;
+      default:
+        return state;
     }
-}
+  }
 
 export default combineReducers({ user, cars, makes })
